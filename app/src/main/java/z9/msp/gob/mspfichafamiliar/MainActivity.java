d3298c7bac5e7adabbd90d48d645d991efcd9515
henryRafaelAvila <henry1894@hotmail.es>
2017-04-01 23:12:59 -0500

    test c

--- app/src/main/java/z9/msp/gob/mspfichafamiliar/MainActivity.java
+++ app/src/main/java/z9/msp/gob/mspfichafamiliar/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         //pruebas
-        //<code></code>
+        //<code></code>find
         return true;
     }
 
