54b5c5174c2f402ae3ca44edec788e0184225936
henryRafaelAvila <henry1894@hotmail.es>
2017-04-03 18:04:03 -0500

    carga spinner

--- persistencia/src/main/java/z9/msp/gob/persistencia/DatabaseHandler.java
+++ persistencia/src/main/java/z9/msp/gob/persistencia/DatabaseHandler.java
@@ -47,4 +47,11 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         Cursor cursor = db.rawQuery(selectQuery, null);
         return  cursor;
     }
+    public Cursor getAllTransporte() {
+        String selectQuery = "SELECT * FROM cocina";
+        SQLiteDatabase db = this.getWritableDatabase();
+        List<String> spinnerContent = new ArrayList<String>();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        return  cursor;
+    }
 }
